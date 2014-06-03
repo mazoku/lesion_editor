deb9eede365ad98daa5d5023c288a177343f79a0
Tomas Ryba - Ubuntu <rybato@gmail.com>
2014-06-03 15:39:38 +0200

    Estimating pdfs - included hack to move hypo and especially hyper pdf furher from the mu_healthy.  Able to do multilabel segmentation. Prepared to segment in 3D.  TODO: put all parameters to ?one? dictionary.

--- graph_cut.py
+++ graph_cut.py
@@ -7,10 +7,12 @@ __author__ = 'tomas'
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage.morphology as scindimor
+import scipy.stats as scista
 
 import skimage.segmentation as skiseg
 import skimage.morphology as skimor
 import skimage.filter as skifil
+import skimage.exposure as skiexp
 
 import cv2
 import pygco
@@ -18,7 +20,7 @@ import pygco
 import tools
 
 #-------------------------------------------------------------
-def load_data(slice_idx=33):
+def load_data(slice_idx=-1):
     # 33 ... hypodense
     # 41 ... small tumor
     # 138 ... hyperdense
@@ -28,13 +30,14 @@ def load_data(slice_idx=33):
     o_data = np.load('input_orig_data.npy')
     mask = np.load('mask.npy')
 
-    data_s = data[slice_idx, :, :]
-    o_data_s = o_data[slice_idx, :, :]
-    mask_s = mask[slice_idx, :, :]
+    if slice_idx != -1:
+        data_s = data[slice_idx, :, :]
+        o_data_s = o_data[slice_idx, :, :]
+        mask_s = mask[slice_idx, :, :]
 
-    data_bbox, _ = tools.crop_to_bbox(data_s, mask_s)
-    o_data_bbox, _ = tools.crop_to_bbox(o_data_s, mask_s)
-    mask_bbox, _ = tools.crop_to_bbox(mask_s, mask_s)
+        data, _ = tools.crop_to_bbox(data_s, mask_s)
+        o_data, _ = tools.crop_to_bbox(o_data_s, mask_s)
+        mask, _ = tools.crop_to_bbox(mask_s, mask_s)
 
     # plt.figure()
     # plt.subplot(131), plt.imshow(data_bbox, 'gray')
@@ -42,30 +45,288 @@ def load_data(slice_idx=33):
     # plt.subplot(133), plt.imshow(mask_bbox, 'gray')
     # plt.show()
 
-    return data_bbox, o_data_bbox, mask_bbox
-
-#-------------------------------------------------------------
-def run(slice_idx, sigma):
-    data, data_o, mask = load_data(slice_idx)
-
-    data_s = skifil.gaussian_filter(data_o, sigma)
-
-    #imd = np.absolute(im - ims)
-    data_d = data_o - data_s
-
-    data_d = np.where(data_d < 0, 0, data_d)
+    return data, o_data, mask
 
 
-    plt.figure()
-    plt.subplot(131), plt.imshow(data_o, 'gray', vmin=0, vmax=255)
-    # plt.subplot(132), plt.imshow(imd, 'gray', vmin=0)
-    plt.subplot(132), plt.imshow(data_d, 'gray')
-    plt.subplot(133), plt.imshow(data_s, 'gray', vmin=0, vmax=255)
+def estimate_healthy_pdf(data, mask, tv_weight, perc, k_std_l, simple_estim=False, show_me=False):
+    # data = tools.smoothing_tv(data.astype(np.uint8), tv_weight)
+    ints = data[np.nonzero(mask)]
+    hist, bins = skiexp.histogram(ints, nbins=256)
+    if simple_estim:
+        mu, sigma = scista.norm.fit(ints)
+    else:
+        ints = data[np.nonzero(mask)]
+
+        n_pts = mask.sum()
+        perc_in = n_pts * perc
+
+        peak_idx = np.argmax(hist)
+        n_in = hist[peak_idx]
+        win_width = 0
+
+        while n_in < perc_in:
+            win_width += 1
+            n_in = hist[peak_idx - win_width:peak_idx + win_width].sum()
+
+        idx_start = bins[peak_idx - win_width]
+        idx_end = bins[peak_idx + win_width]
+        inners_m = np.logical_and(ints > idx_start, ints < idx_end)
+        inners = ints[np.nonzero(inners_m)]
+
+        # liver pdf -------------
+        mu = bins[peak_idx]
+        sigma = k_std_l * np.std(inners)
+
+    rv = scista.norm(mu, sigma)
+
+    if show_me:
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(bins, hist)
+        plt.title('histogram with max peak')
+        plt.hold(True)
+        plt.plot([mu, mu], [0, hist.max()], 'g')
+        ax = plt.axis()
+        plt.axis([0, 256, ax[2], ax[3]])
+        # plt.subplot(212), plt.plot(bins, rv_l.pdf(bins), 'g')
+        x = np.arange(0, 256, 0.1)
+        plt.subplot(212), plt.plot(x, rv.pdf(x), 'g')
+        plt.hold(True)
+        plt.plot(mu, rv.pdf(mu), 'go')
+        ax = plt.axis()
+        plt.axis([0, 256, ax[2], ax[3]])
+        plt.title('estimated normal pdf of healthy parenchym')
+        # plt.show()
+
+    return mu, sigma, rv
+
+
+def estimate_outlier_pdf(data, mask, rv_healthy, prob_w, outlier_type, hack_it, show_me=False):
+    probs = rv_healthy.pdf(data) * mask
+    max_prob = rv_healthy.pdf(rv_healthy.mean())
+    # print 'max_prob = %.3f' % max_prob
+    prob_t = prob_w * max_prob
+
+    ints_out_m = probs < prob_t * mask
+
+    ints_out = data[np.nonzero(ints_out_m)]
+    hist, bins = skiexp.histogram(ints_out, nbins=256)
+
+    if outlier_type == 'hypo':
+        ints = ints_out[np.nonzero(ints_out < rv_healthy.mean())]
+    elif outlier_type == 'hyper':
+        ints = ints_out[np.nonzero(ints_out > rv_healthy.mean())]
+    else:
+        print 'Wrong outlier specification.'
+        return
+
+    mu, sigma = scista.norm.fit(ints)
+
+    # hack for moving pdfs of hypo and especially of hyper furhter from the pdf of healthy parenchyma
+    if outlier_type == 'hyper':
+        mu += hack_it
+        sigma += hack_it
+    elif outlier_type == 'hyper':
+        mu -= hack_it
+        sigma -= hack_it
+    #-----------
+    rv = scista.norm(mu, sigma)
+
+    if show_me:
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(bins, hist)
+        plt.title('histogram with max peak')
+        plt.hold(True)
+        plt.plot([mu, mu], [0, hist.max()], 'g')
+        ax = plt.axis()
+        plt.axis([0, 256, ax[2], ax[3]])
+        # plt.subplot(212), plt.plot(bins, rv_l.pdf(bins), 'g')
+        x = np.arange(0, 256, 0.1)
+        plt.subplot(212), plt.plot(x, rv.pdf(x), 'g')
+        plt.hold(True)
+        plt.plot(mu, rv.pdf(mu), 'go')
+        ax = plt.axis()
+        plt.axis([0, 256, ax[2], ax[3]])
+        plt.title('estimated normal pdf of %sdense obejcts' % outlier_type)
+        # plt.show()
+
+    return mu, sigma, rv
+
+
+def get_unaries(data, mask, tv_weight, perc, prob_w, k_std_h, k_std_t, healthy_simple_estim, hack_hypo, hack_hyper, show_me):
+    # liver pdf ------------
+    print 'estimating pdf of healthy parenchym...'
+    mu_h, sigma_h, rv_healthy = estimate_healthy_pdf(data, mask, tv_weight, perc, k_std_h, healthy_simple_estim, False)
+    print 'liver pdf: mu = ', mu_h, ', sigma = ', sigma_h
+
+    # mask_e = skimor.binary_erosion(mask, np.ones((5, 5)))
+    # liver_probs = rv_healthy.pdf(data) * mask_e
+    # plt.figure()
+    # plt.subplot(121), plt.imshow(liver_probs, 'gray'), plt.colorbar()
+    # plt.subplot(122), plt.imshow(liver_probs > 0.05, 'gray')
+    # plt.show()
+
+    # hypodense pdf ------------
+    print 'estimating pdf of hypodense objects...'
+    # mu_hypo, sigma_hypo, rv_hypo = estimate_hypo_pdf(data, mask, rv_healthy, show_me)
+    mu_hypo, sigma_hypo, rv_hypo = estimate_outlier_pdf(data, mask, rv_healthy, prob_w, 'hypo', hack_hypo, False)
+    print 'hypodense pdf: mu = ', mu_hypo, ', sigma= ', sigma_hypo
+
+    # hyperdense pdf ------------
+    print 'estimating pdf of hyperdense objects...'
+    # mu_hyper, sigma_hyper, rv_hyper = estimate_hyper_pdf(data, mask, rv_healthy, show_me)
+    mu_hyper, sigma_hyper, rv_hyper = estimate_outlier_pdf(data, mask, rv_healthy, prob_w, 'hyper', hack_hyper, False)
+    print 'hyperdense pdf: mu = ', mu_hyper, ', sigma= ', sigma_hyper
+
+    if show_me:
+        ints = data[np.nonzero(mask)]
+        hist, bins = skiexp.histogram(ints, nbins=256)
+        x = np.arange(0, 255, 0.01)
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(bins, hist)
+        ax = plt.axis()
+        plt.axis([0, 256, ax[2], ax[3]])
+        plt.title('histogram of input data')
+        plt.subplot(212)
+        plt.plot(x, rv_hypo.pdf(x), 'm')
+        plt.hold(True)
+        plt.plot(x, rv_healthy.pdf(x), 'b')
+        plt.plot(x, rv_hyper.pdf(x), 'g')
+        ax = plt.axis()
+        plt.axis([0, 256, ax[2], ax[3]])
+        plt.title('histogram of input data')
+        plt.legend(['hypodense pdf', 'healthy pdf', 'hyperdense pdf'])
+        plt.show()
+
+    # unaries_l = rv_l.pdf(data)
+    # unaries_t = rv_t.pdf(data)
+    if data.ndim == 3:
+        mask_e = tools.eroding3D(mask, skimor.disk(5))
+    else:
+        mask_e = skimor.binary_erosion(mask, np.ones((5, 5)))
+    # mask_e = mask
+    # unaries_bcg = - (rv_healthy.logpdf(data) + rv_hyper.logpdf(data)) * mask_e
+    unaries_healthy = - rv_healthy.logpdf(data)  * mask_e
+    unaries_hyper = - rv_hyper.logpdf(data) * mask_e
+    unaries_hypo = - rv_hypo.logpdf(data) * mask_e
+
+    # # display unary potentials
+    # plt.figure()
+    # plt.subplot(221), plt.imshow(data, 'gray')
+    # plt.subplot(223), plt.imshow(unaries_l, 'gray'), plt.title('liver unaries'), plt.colorbar()
+    # plt.subplot(224), plt.imshow(unaries_t, 'gray'), plt.title('tumor unaries'), plt.colorbar()
+    # # plt.show()
+    #
+    # display estimated pdf of normal distribution
+    # ints = data[np.nonzero(mask)]
+    # hist, bins = skiexp.histogram(ints, nbins=256)
+    # plt.figure()
+    # plt.subplot(211)
+    # plt.plot(bins, hist)
+    # plt.hold(True)
+    # plt.plot([mu_h, mu_h], [0, hist.max()], 'g')
+    # plt.plot([mu_hypo, mu_hypo], [0, hist.max()], 'm')
+    # plt.subplot(212), plt.plot(bins, rv_healthy.pdf(bins), 'g')
+    # plt.subplot(212), plt.plot(bins, rv_hypo.pdf(bins), 'm')
+    # plt.hold(True)
+    # plt.plot(mu_h, rv_healthy.pdf(mu_h), 'go')
+    # plt.plot(mu_hypo, rv_hypo.pdf(mu_hypo), 'mo')
+    # plt.show()
+
+    # # display histogram with borders of points from which the norm pdf is estimated
+    # plt.figure()
+    # plt.subplot(211), plt.plot(bins, hist)
+    # plt.hold(True)
+    # plt.plot(bins[peak_idx], hist[peak_idx], 'ro')
+    # plt.plot([bins[peak_idx - win_width], bins[peak_idx - win_width]], [0, hist[peak_idx]], 'r')
+    # plt.plot([bins[peak_idx + win_width], bins[peak_idx + win_width]], [0, hist[peak_idx]], 'r')
+    # plt.subplot(212), plt.plot(bins, rv.pdf(bins))
+    # plt.hold(True)
+    # plt.plot(bins[peak_idx], rv.pdf(bins[peak_idx]), 'ro')
+    # plt.show()
+
+    # unaries = np.dstack((unaries_hyper, unaries_hypo)).astype(np.int32)
+    unaries = np.dstack((unaries_hypo, unaries_healthy, unaries_hyper)).astype(np.int32)
+    return unaries
+
+
+def run(slice_idx, sigma, alpha, beta, tv_weight, perc, prob_w, k_std_h, k_std_t, healthy_simple_estim, hack_hypo, hack_hyper, show_me):
+    _, data_o, mask = load_data(slice_idx)
+    # data_o = cv2.imread('/home/tomas/Dropbox/images/medicine/hypodense_bad2.png', 0).astype(np.float)
+
+    # data_s = skifil.gaussian_filter(data_o, sigma)
+    #
+    # #imd = np.absolute(im - ims)
+    # data_d = data_o - data_s
+    #
+    # data_d = np.where(data_d < 0, 0, data_d)
+
+    print 'calculating unary potentials...'
+    # create unaries
+    # unaries = data_d
+    # # as we convert to int, we need to multipy to get sensible values
+    # unaries = (1 * np.dstack([unaries, -unaries]).copy("C")).astype(np.int32)
+    unaries = beta * get_unaries(data_o, mask, tv_weight, perc, prob_w, k_std_h, k_std_t, healthy_simple_estim, hack_hypo, hack_hyper, show_me)
+    n_labels = unaries.shape[2]
+
+    print 'calculating pairwise potentials...'
+    # create potts pairwise
+    pairwise = -alpha * np.eye(n_labels, dtype=np.int32)
+
+    print 'deriving graph edges...'
+    # use the gerneral graph algorithm
+    # first, we construct the grid graph
+    inds = np.arange(data_o.size).reshape(data_o.shape)
+    if data_o.ndim == 2:
+        horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
+        vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
+        edges = np.vstack([horz, vert]).astype(np.int32)
+    elif data_o.ndim == 3:
+        # horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
+        # vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
+        horz = np.c_[inds[:, :, :-1].ravel(), inds[:, :, 1:].ravel()]
+        vert = np.c_[inds[:, :-1, :].ravel(), inds[:, 1:, :].ravel()]
+        dept = np.c_[inds[:-1, :, :].ravel(), inds[1:, :, :].ravel()]
+        edges = np.vstack([horz, vert, dept]).astype(np.int32)
+    # deleting edges with nodes outside the mask
+    nodes_in = np.ravel_multi_index(np.nonzero(mask), data_o.shape)
+    rows_inds = np.in1d(edges, nodes_in).reshape(edges.shape).sum(axis=1) == 2
+    edges = edges[rows_inds, :]
+
+    print 'calculating graph cut...'
+    # we flatten the unaries
+    # result_graph = pygco.cut_from_graph(edges, unaries.reshape(-1, 2), pairwise)
+    # print 'tu: ', unaries.reshape(-1, n_labels).shape
+    result_graph = pygco.cut_from_graph(edges, unaries.reshape(-1, n_labels), pairwise)
+    res = result_graph.reshape(data_o.shape)
+
+    res = np.where(mask, res, -1)
+    print '\t...done'
 
     # plt.figure()
-    # plt.imshow(ims, 'gray')
-    plt.show()
+    # plt.subplot(131), plt.imshow(data_o, 'gray', vmin=0, vmax=255)
+    # plt.subplot(132), plt.imshow(data_d, 'gray')
+    # plt.subplot(133), plt.imshow(data_s, 'gray', vmin=0, vmax=255)
+    # plt.show()
 
+    plt.subplot(2, n_labels, 1), plt.title('original')
+    plt.imshow(data_o, 'gray', interpolation='nearest')
+    plt.subplot(2, n_labels, 2), plt.title('graph cut')
+    plt.imshow(res, 'jet', interpolation='nearest', vmin=res.min(), vmax=res.max()), plt.colorbar(ticks=np.unique(res))
+    if n_labels == 2:
+        k = 3
+    else:
+        k = 4
+    plt.subplot(2, n_labels, k), plt.title('unary labels = 0')
+    plt.imshow(unaries[:, :, 0], 'gray', interpolation='nearest'), plt.colorbar()
+    plt.subplot(2, n_labels, k + 1), plt.title('unary labels = 1')
+    plt.imshow(unaries[:, :, 1], 'gray', interpolation='nearest'), plt.colorbar()
+    if n_labels == 3:
+        plt.subplot(2, n_labels, k + 2), plt.title('unary labels = 2')
+        plt.imshow(unaries[:, :, 2], 'gray', interpolation='nearest'), plt.colorbar()
+    plt.show()
 
 
 #-------------------------------------------------------------
@@ -73,8 +334,20 @@ if __name__ == '__main__':
     # 33 ... hypodense
     # 41 ... small tumor
     # 138 ... hyperdense
+    # -1 ... all data
     slice_idx = 33
+    sigma = 10  # sigma for gaussian blurr
+    alpha = 2  # weightening parameter for pairwise term
+    beta = 1  # weightening parameter for unary term
+    perc = 0.3  # what portion of liver parenchym around peak is used to calculate std of liver normal pdf
+    k_std_h = 3  # weighting parameter for sigma of normal distribution of healthy parenchym
+    k_std_t = 3  # weighting parameter for sigma of normal distribution of tumor
+    tv_weight = 0.05  # weighting parameter for total variation filter
+    healthy_simple_estim = False  # simple healthy parenchym pdf estimation from all data
+    prob_w = 0.5  # prob_w * max_prob is a threshold for data that will be used for estimation of other pdfs
+    hack_hypo = 0  # hard move of mean and sigma of hypodense pdf (mean to the left)
+    hack_hyper = 5  # hard move of mean and sigma of hyperdense pdf (mean to the right)
 
-    sigma = 100  # sigma for gaussian blurr
+    show_me = True  # debug visualization
 
-    run(slice_idx, sigma)
+    run(slice_idx, sigma, alpha, beta, tv_weight, perc, prob_w, k_std_h, k_std_t, healthy_simple_estim, hack_hypo, hack_hyper, show_me)
--- upo.py
+++ upo.py
@@ -13,29 +13,12 @@ o_data = np.load('input_orig_data.npy')
 mask = np.load('mask.npy')
 n_slices = mask.shape[0]
 
-
-# o_data_s = o_data[slice_idx, :, :]
-# mask_s = mask[slice_idx, :, :]
-
-# ints = o_data_s[np.nonzero(mask_s)].astype(np.int)
-# hist, bins = skiexp.histogram(ints, nbins=256)
-
-ints_o = o_data[np.nonzero(mask)].astype(np.int)
+ints_o = o_data[np.nonzero(mask)]
 hist_o, bins_o = skiexp.histogram(ints_o, nbins=256)
 
 mu, sigma = scista.norm.fit(ints_o)
+print mu, sigma
 rv_h = scista.norm(mu, sigma)
-# rv2 = scista.norm(mu, sigma / 2)
-
-# x = np.linspace(rv.ppf(0.01), rv.ppf(0.99), 100)
-# plt.figure()
-# plt.subplot(211), plt.plot(bins_o, hist_o)
-# ax = plt.axis()
-# plt.axis([0, 256, ax[2], ax[3]])
-# plt.subplot(212), plt.plot(x, rv.pdf(x), 'r-')
-# ax = plt.axis()
-# plt.axis([0, 256, ax[2], ax[3]])
-# plt.show()
 
 # mask_e = tools.eroding3D(mask, skimor.disk(5))
 mask_e = mask
@@ -62,6 +45,13 @@ rv_hypo = scista.norm(g[0], g[1])
 g = scista.norm.fit(ints_hyper)
 rv_hyper = scista.norm(g[0], g[1])
 
+# plt.figure()
+# plt.plot(x, rv_hypo.pdf(x), 'b')
+# plt.hold(True)
+# plt.plot(x, rv_h.pdf(x) + rv_hyper.pdf(x), 'r')
+# plt.legend(['hypo as norm', 'healthy + hyper, both as norm'])
+# plt.show()
+
 plt.figure()
 plt.subplot(211)
 plt.plot(bins_out, hist_out)
@@ -79,86 +69,16 @@ ax = plt.axis()
 plt.axis([0, 256, ax[2], ax[3]])
 plt.legend(['ouliers as dgamma', 'healthy as norm', 'hypo as norm', 'hyper as norm'])
 plt.show()
-
-probs_l = rv_h.pdf(o_data)
-probs_o = rv_outs.pdf(o_data)
-probs_hypo = rv_hypo.pdf(o_data)
-probs_hyper = rv_hyper.pdf(o_data)
-plt.figure()
-plt.subplot(321), plt.imshow(o_data[slice_idx, :, :], 'gray')
-plt.subplot(323), plt.imshow(probs_l[slice_idx, :, :], 'gray'), plt.title('helthy as norm')
-plt.subplot(324), plt.imshow(probs_o[slice_idx, :, :], 'gray'), plt.title('outliers as dgamma')
-plt.subplot(325), plt.imshow(probs_hypo[slice_idx, :, :], 'gray'), plt.title('hypo as norm')
-plt.subplot(326), plt.imshow(probs_hyper[slice_idx, :, :], 'gray'), plt.title('hyper as norm')
-plt.show()
-
-# h_hyper, b_hyper = skiexp.histogram(ints_hyper, nbins=256)
-# h_hypo, b_hypo = skiexp.histogram(ints_hypo, nbins=256)
-# ints_hypo_res = skiexp.rescale_intensity(ints_hypo, (ints_hypo.min(), ints_hypo.max()), (ints_hypo.max(), ints_hypo.min()))
-# h_hypo_res, b_hypo_res = skiexp.histogram(ints_hypo_res, nbins=256)
-
-# plt.figure()
-# plt.plot(b_hyper, h_hyper, 'b')
-# plt.hold(True)
-# plt.plot(b_hypo, h_hypo, 'm')
-# plt.plot(b_hypo_res, h_hypo_res, 'g')
-# plt.show()
-
-# shape_hyper, loc_hyper, scale_hyper = scista.lognorm.fit(ints_hyper)
-# rv_hyper = scista.lognorm(shape_hyper, loc=loc_hyper, scale=scale_hyper)
-# x = np.linspace(rv_hyper.ppf(0.001), rv_hyper.ppf(0.999), 300)
-
-# ints_hypo_res = skiexp.rescale_intensity(ints_hypo, (ints_hypo.min(), ints_hypo.max()), (ints_hypo.max(), ints_hypo.min()))
-# shape_hypo, loc_hypo, scale_hypo = scista.lognorm.fit(ints_hypo_res)
-
-plt.figure()
-plt.plot(x, rv_hypo.pdf(x))
-plt.show()
-
-# plt.figure()
-# plt.subplot(211), plt.plot(x, rv_hyper.pdf(x)), plt.title('hyperdense pdf')
-# plt.subplot(212), plt.plot(x, rv_hypo.pdf(x)), plt.title('hypodense pdf')
-# plt.show()
-
-# plt.figure()
-# plt.plot(x, rv_hyper.pdf(x))
-# plt.show()
-
-# plt.figure()
-# plt.subplot(221), plt.imshow(o_data[slice_idx, :, :], 'gray')
-# plt.subplot(222), plt.imshow(probs[slice_idx, :, :], 'gray'), plt.colorbar()
-# plt.subplot(223), plt.imshow(probs[slice_idx, :, :] > prob_t, 'gray')
-# plt.show()
-
-# for i in range(n_slices):
-#     # probs = rv.pdf(o_data[i, :, :]) * mask_e
-#     plt.figure()
-#     plt.subplot(121), plt.imshow(o_data[i, :, :], 'gray')
-#     plt.subplot(122), plt.imshow(probs[i, :, :], 'gray'), plt.colorbar()
-#     plt.show()
-
-# HISTOGRAM PO REZECH -----------------------------------------------------------------
-# min_size = 100
 #
-# for i in range(n_slices):
-#     print 'slice #%i/%i...' % (i, n_slices)
-#     data_s = o_data[i, :, :]
-#     mask_s = mask[i, :, :]
-#
-#     ints = data_s[np.nonzero(mask_s)].astype(np.int)
-#     print '\tsize: ', ints.size
-#     if ints.size > min_size:
-#         hist, bins = skiexp.histogram(ints, nbins=256)
-#
-#         data_bbox, _ = tools.crop_to_bbox(data_s, mask_s)
-#         plt.figure()
-#         plt.subplot(211), plt.imshow(data_bbox, 'gray', interpolation='nearest')
-#         plt.subplot(212), plt.plot(bins, hist), plt.axis([0, 256, 0, hist.max()])
-#         plt.show()
-#     else:
-#         print '\tto small to estimate anything'
-#--------------------------------------------------------------------------------------
+# probs_l = rv_h.pdf(o_data)
+# probs_o = rv_outs.pdf(o_data)
+# probs_hypo = rv_hypo.pdf(o_data)
+# probs_hyper = rv_hyper.pdf(o_data)
+# plt.figure()
+# plt.subplot(321), plt.imshow(o_data[slice_idx, :, :], 'gray')
+# plt.subplot(323), plt.imshow(probs_l[slice_idx, :, :], 'gray'), plt.title('helthy as norm')
+# plt.subplot(324), plt.imshow(probs_o[slice_idx, :, :], 'gray'), plt.title('outliers as dgamma')
+# plt.subplot(325), plt.imshow(probs_hypo[slice_idx, :, :], 'gray'), plt.title('hypo as norm')
+# plt.subplot(326), plt.imshow(probs_hyper[slice_idx, :, :], 'gray'), plt.title('hyper as norm')
+# plt.show()
 
-# mu =
-# sigma =
-# rv = scista.lognorm

a9c0862b1b79fd9b9d96cff82a383d75b66fec75
Tomas Ryba <rybato@gmail.com>
2015-10-08 08:32:11 +0200

    Oprava setupu.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/mazoku/lesion_editor',
     author='Tomas Ryba',
     author_email='rybato@gmail.com',
@@ -48,7 +48,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    # packages=find_packages(exclude=['dist',  'docs', 'tests*']),
+    packages=find_packages(exclude=['dist', 'docs', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
